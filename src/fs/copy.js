ba15818562867e6177e7737d7e2b2d2381915df3
Andrii Ferenchuk <ferenchuk.a@hotmail.com>
2025-04-27 00:07:51 +0300

    feat: implement file copy logic with error handling and add new files

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import fs from 'node:fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const copy = async () => {
     // Write your code here 
-};
+    const sourceDir = path.join(__dirname, 'files')
+    const destDir = path.join(__dirname, 'files_copy')
 
-await copy();
+    try {
+        await fs.access(sourceDir)
+        try {
+            await fs.access(destDir)
+            throw new Error('FS operation failed')
+        } catch (error) {
+            if (error.code !== 'ENOENT') {
+                throw new Error('FS operation failed')
+            }
+        }
+        await fs.cp(sourceDir, destDir, { recursive: true, errorOnExist: true, force: false })
+    } catch (error) {
+        throw new Error('FS operation failed')
+    }
+}
+
+await copy()
