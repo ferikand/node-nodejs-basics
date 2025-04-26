16fba16d721bd57e9168f293489e930bd4b632a7
Andrii Ferenchuk <ferenchuk.a@hotmail.com>
2025-04-26 23:50:54 +0300

    feat: implement file creation logic and add fresh.txt

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from 'node:fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+
 const create = async () => {
     // Write your code here 
-};
 
-await create();
+    const filePath = path.join(__dirname, 'files', 'fresh.txt')
+    const content = 'I am fresh and young'
+
+    try {
+        await fs.writeFile(filePath, content, { flag: 'wx' })
+    } catch (error) {
+        console.error(error)
+        throw new Error('FS operation failed')
+    }
+
+}
+
+await create()
