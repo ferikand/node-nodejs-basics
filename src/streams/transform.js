15762ed4739d58c31d9884f43a91b5bb73077d3b
Andrii Ferenchuk <ferenchuk.a@hotmail.com>
2025-04-27 18:17:49 +0300

    feat: xtransform task completed

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+// implement function that reads data from process.stdin, reverses text using Transform Stream and then writes it into process.stdout
+import { Transform } from 'node:stream'
+
 const transform = async () => {
     // Write your code here 
+    
+    const reverseTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversedChunk = chunk.toString().split('').reverse().join('')
+            callback(null, reversedChunk)
+        }
+    })
+
+    process.stdin.pipe(reverseTransform).pipe(process.stdout)
 };
 
 await transform();
