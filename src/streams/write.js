9f33dc48a71c2fbc918e842268b080124581fa2e
Andrii Ferenchuk <ferenchuk.a@hotmail.com>
2025-04-27 18:03:21 +0300

    feat: implement write content to file logic

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+// implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
+import { createWriteStream } from 'node:fs'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+
 const write = async () => {
     // Write your code here 
-};
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt')
 
-await write();
+    try {
+        const stream = createWriteStream(filePath, { flags: 'a' })
+        process.stdin.pipe(stream)
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.log(err.message)
+        }
+    }
+}
+
+await write()
