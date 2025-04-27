e058fa2076a8890ac4a314f75854b961cb56989c
Andrii Ferenchuk <ferenchuk.a@hotmail.com>
2025-04-27 17:37:41 +0300

    feat: implement reads file content via stream and print logic

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,27 @@
+// read.js - implement function that reads file fileToRead.txt content using Readable Stream and prints it's content into process.stdout
+
+import { createReadStream } from 'node:fs'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+import { access } from 'node:fs/promises'
+
 const read = async () => {
     // Write your code here 
-};
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt')
 
-await read();
+    try {
+        await access(filePath)
+        const stream = createReadStream(filePath, { encoding: 'utf-8' })
+        for await (const chunk of stream) {
+            process.stdout.write(chunk)
+        }
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.log(err.message)
+        }
+    }
+}
+
+await read()
