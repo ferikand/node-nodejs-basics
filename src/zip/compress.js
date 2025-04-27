e896a1c50dfd185c58fa7799c0c8b5bc1da3306e
Andrii Ferenchuk <ferenchuk.a@hotmail.com>
2025-04-27 18:37:20 +0300

    feat: implement logic to compress file

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,30 @@
+// implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+import { createGzip } from 'node:zlib'
+import { createReadStream, createWriteStream } from 'node:fs'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+import { pipeline } from 'node:stream/promises'
+
 const compress = async () => {
     // Write your code here 
-};
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+    const filePath = path.join(__dirname, 'files', 'fileToCompress.txt')
+    const archivePath = path.join(__dirname, 'files', 'archive.gz')
 
-await compress();
+    const readStream = createReadStream(filePath)
+    const gzipStream = createGzip()
+    const writeStream = createWriteStream(archivePath)
+
+    try {
+        await pipeline(readStream, gzipStream, writeStream)
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.log(err.message)
+        } else {
+            console.error('Pipeline failed', err)
+        }
+    }
+}
+
+await compress()
